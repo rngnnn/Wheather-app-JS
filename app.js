e00f43ee205c686950d3b112e39b7e221c482dce
rngnnn <rngnnnclk@gmail.com>
2024-06-26 15:49:56 +0200

    cannot read

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
-//figcaption en alttaki hava durumu yazisi
-//.city-temp=derece
-//.city
-//sup=tag
-//.city-name
-//.city-name
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+const cities = document.querySelector(".cities")
 
-const input=document.querySelector("input")
-const button=document.querySelector("button")
+let arrayUsed = []
+let inputValue= ""
 
-
-let inputValue =""
-
-let apiLink=`https://api.openweathermap.org/data/2.5/weather?q={inputValue}&appid=a17bc3f5ae92ad5fdbaad666100b6f23`
-
-button.onclick=(e)=>{
-
-inputValue= input.value.toString()
-
-}
+let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=Ankara&appid=a17bc3f5ae92ad5fdbaad666100b6f23`
 
 
-fetch(apiLink)
 
+
+fetch (apiLink)
 .then((resp)=>resp.json())
 .then((data)=>{
 
-    arrayUsed=data
-    showScreen(data)
+    arrayUsed = data
+    ekranaBasma(data)
 })
-console.log(fetch);
-
-function showScreen(a){
 
 
-
+button.onclick=(e)=>{
+    e.preventDefault()
+    inputValue = input.value
 }
+
+
+
+function ekranaBasma (veri){
+let newLi = document.createElement("li")
+    newLi.appendChild(cities)
+
+    newLi.innerHTML += `
+    
+    <h2 class="city-name" data-name="${veri.name},${veri.sys.country}">
+             <span>${veri.name}</span>
+                <sup>${veri.sys.country}</sup>
+           </h2>
+           <div class="city-temp">${Math.round(veri.main.temp)}<sup>°C</sup></div>
+           <figure>
+                 <img class="city-icon" src="https://openweathermap.org/img/wn/${veri.weather[0].icon}@2x.png">
+                 <figcaption>${veri.weather[0].description}</figcaption>
+           </figure>
+    `;
+}
+ekranaBasma();
